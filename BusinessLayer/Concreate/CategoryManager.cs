e280cb74e8af88075d61846e78413a0189c8e4b8
uguryaman <77986488+uguryaman@users.noreply.github.com>
2021-05-18 21:54:31 +0300

    Mvc katmanlar
    Murat Yücedağ sertifika ödev1/4

---
+++ BusinessLayer/Concreate/CategoryManager.cs
@@ -0,0 +1,32 @@
+﻿using DataAccessLayer.Concreate.Repositories;
+using EntityLayer.Concreate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concreate
+{
+    public class CategoryManager
+    {
+        GenericRepository<Category> repo = new GenericRepository<Category>();
+
+        public List<Category> GetAllBL()
+        {
+            return repo.List();
+        }
+        public void CategoryAddBl(Category p)
+        {
+            if (p.CategoryName=="" || p.CategoryName.Length <= 3 || p.CategoryName.Length >= 51 || p.CategoryDescription == "")
+            {
+                //hata mesajı
+            }
+            else
+            {
+                repo.Insert(p);
+            }
+            
+        }
+    }
+}
---
+++ DataAccessLayer/Concreate/Context.cs
@@ -0,0 +1,22 @@
+﻿using EntityLayer.Concreate;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concreate
+{
+    public class Context: DbContext
+    {
+        internal object _object;
+
+        public DbSet<About> Abouts { get; set; } 
+        public DbSet<Category> Categories  { get; set; } 
+        public DbSet<Content> Contents  { get; set; } 
+        public DbSet<Heading> Headings  { get; set; } 
+        public DbSet<Writer> Writers  { get; set; } 
+
+    }
+}
